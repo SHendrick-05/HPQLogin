331a2805c8adace118d57f9f9d405336d72fcd36
Sean <seanhendrick05@gmail.com>
2021-06-08 12:28:35 +0100

    Add files via upload

---
+++ HPQ Project/Security/Conversions.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace HPQ_Project
+{
+    class Conversions
+    {
+        private static readonly uint[] _lookup32 = CreateLookup32();
+
+        private static uint[] CreateLookup32()
+        {
+            var result = new uint[256];
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("X2");
+                result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+            }
+            return result;
+        }
+
+        internal static string ByteArrayToHexViaLookup32(byte[] bytes)
+        {
+            var lookup32 = _lookup32;
+            var result = new char[bytes.Length * 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                var val = lookup32[bytes[i]];
+                result[2 * i] = (char)val;
+                result[2 * i + 1] = (char)(val >> 16);
+            }
+            return new string(result);
+        }
+
+        internal static string rightShift(string key, int shift)
+        {
+            shift %= key.Length;
+            return key.Substring(key.Length - shift) + key.Substring(0, key.Length - shift);
+        }
+
+        private readonly static Dictionary<char, byte> hexmap = new Dictionary<char, byte>()
+	    {
+		    { 'a', 0xA },{ 'b', 0xB },{ 'c', 0xC },{ 'd', 0xD },
+		    { 'e', 0xE },{ 'f', 0xF },{ 'A', 0xA },{ 'B', 0xB },
+		    { 'C', 0xC },{ 'D', 0xD },{ 'E', 0xE },{ 'F', 0xF },
+		    { '0', 0x0 },{ '1', 0x1 },{ '2', 0x2 },{ '3', 0x3 },
+		    { '4', 0x4 },{ '5', 0x5 },{ '6', 0x6 },{ '7', 0x7 },
+		    { '8', 0x8 },{ '9', 0x9 }
+	    };
+
+        public static byte[] HexToByte(string hex)
+        {
+            byte[] bytesArr = new byte[(hex.Length) / 2];
+
+            char leftNib;
+            char rightNib;
+            int x = 0;
+            for (int i = 0; i < hex.Length; i += 2, x++)
+            {
+                leftNib = hex[i];
+                rightNib = hex[i + 1];
+                bytesArr[x] = (byte)((hexmap[leftNib] << 4) | hexmap[rightNib]);
+            }
+            return bytesArr;
+        }
+    }
+}
---
+++ HPQ Project/Security/Verification.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace HPQ_Project
+{
+    static class Verification
+    {
+        // A function to attempt to register an account, and add it to the database
+        // Returns an integer code.
+        internal static int regHandle(string username, string password, string email)
+        {
+            if (string.IsNullOrWhiteSpace(username) ||
+                string.IsNullOrWhiteSpace(password) ||
+                string.IsNullOrWhiteSpace(email))
+            {
+                return 1; // Error code 1 if either box is empty.
+            }
+
+            if (Databasing.GetAccount(username) != null)
+            {
+                return 3; // Error code 3 if account already exists.
+            }
+            
+            // Check the email
+            string emailPattern = @"^([a-zA-Z0-9]+)(@)([a-zA-Z0-9-]+)(\x2E\w+)+$";
+            Regex emailRGX = new Regex(emailPattern);
+            if (!emailRGX.IsMatch(email)) 
+                return 2; // Error code 2 if email is invalid.
+
+            // Variables
+            byte[] Xarr = new byte[128],
+                   Yarr = new byte[128];
+            BigInteger X = new BigInteger(),
+                       Y = new BigInteger();
+
+            // Password encryption
+            int len = password.Length;
+
+            // Hashing the password via SHA512, to create a 64-byte array result.
+            byte[] data = Encoding.Unicode.GetBytes(password);
+            byte[] result = new byte[64];
+            SHA512 shaM = SHA512.Create();
+            result = shaM.ComputeHash(data);
+
+            // Concaternate it to itself, and then convert to a hex string
+            byte[] result1024 = result.Concat(result).ToArray();
+            string result256h = Conversions.ByteArrayToHexViaLookup32(result1024);
+
+            // Right shift the hex string by the length of the password, then convert back to a byte array
+            string shifted256h = Conversions.rightShift(result256h, len);
+            byte[] shifted1024 = Conversions.HexToByte(shifted256h);
+
+            // Generate two 1024-bit primes.
+            X = Primes.genPrime();
+            Y = Primes.genPrime();
+            Yarr = Y.ToByteArray();
+
+            // Transform the prime Y by HK' (shifted hash) by a bitwise XOR
+            byte[] transformedY = new byte[128];
+            for (int i = 0; i < 128; i++)
+            {
+                int transformInt = Yarr[i] ^ shifted1024[i];
+                transformedY[i] = Convert.ToByte(transformInt);
+            }
+            // Get the transformed Y integer, and the large prime Z.
+            BigInteger trnsY = new BigInteger(transformedY);
+            BigInteger Z = BigInteger.Multiply(X, Y);
+
+            // Add this to the database and return a success code.
+            Databasing.AddAccount(username, email, Z.ToString(), trnsY.ToString());
+            return 0;
+        }
+
+        ///
+        ///
+
+
+        // A function to attempt to login as an account, checking it agianst the database.
+        internal static int logHandle(string UName, string PWord)
+        {
+            // Make sure no inputs are empty...
+            if (string.IsNullOrWhiteSpace(UName) ||
+                string.IsNullOrWhiteSpace(PWord))
+            {
+                return 1;
+            }
+
+            // Get the referenced account (and make sure it exists!)
+            Account acc = Databasing.GetAccount(UName);
+            if (acc == null) return 2;
+
+            // Get length of password used for verification
+            int len = PWord.Length;
+
+            // Hashing the password via SHA-512 for a 64-bit byte array
+            byte[] data = Encoding.Unicode.GetBytes(PWord);
+            byte[] result = new byte[64];
+            SHA512 shaM = SHA512.Create();
+            result = shaM.ComputeHash(data);
+
+            // Get the concatenated hash and then circular shift it to get HK'.
+            byte[] result1024 = result.Concat(result).ToArray();
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
+
+/*
+ * Register result codes:
+ * 0 = Success
+ * 1 = One or more boxes empty
+ * 2 = Email invalid
+ * 3 = Account already exists
+ * 
+ * Login result codes:
+ * 0 = Success
+ * 1 = Username/pass empty
+ * 2 = Account does not exist
+ * 3 = Invalid username/password
+*/
