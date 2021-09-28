46c3dfcc37378fbb262662c5d1444c403f4d29c0
Sean <seanhendrick05@gmail.com>
2021-09-28 08:53:02 +0100

    Upload new changes

--- HPQ Project/Databasing.cs
+++ HPQ Project/Databasing.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
 
 namespace HPQ_Project
 {
@@ -24,19 +25,49 @@ namespace HPQ_Project
 
     internal static class Databasing
     {
-        public static string dbPath = Properties.Resources.databasePath;
+        public static string dbPath = "database.xlsx";//Properties.Resources.databasePath;
+
+        // Constructor method, to verify existence of the database
+        static Databasing()
+        {
+            if (!System.IO.File.Exists(dbPath))
+            {
+                // Database file not found, create one.
+                var exApp = new Excel.Application();
+                var wb = exApp.Workbooks.Add();
+
+                // Format columns A-D as text
+                exApp.get_Range("A:A").NumberFormat = "@";
+                exApp.get_Range("B:B").NumberFormat = "@";
+                exApp.get_Range("C:C").NumberFormat = "@";
+                exApp.get_Range("D:D").NumberFormat = "@";
+
+                // Small labels for manual requirements
+                exApp.get_Range("A1").Value = "Username";
+                exApp.get_Range("B1").Value = "Email";
+                exApp.get_Range("C1").Value = "Z";
+                exApp.get_Range("D1").Value = "Y'";
+                
+                // Save the result
+                string x = Path.GetFullPath(dbPath);
+                wb.SaveAs(Path.GetFullPath(dbPath));
+                exApp.Quit();
+            }
+        }
 
         // A global list of accounts used to save to and load from the database
         private static List<Account> Accounts = new List<Account>();
 
         // DERIVED METHODS //
 
+        // Add an account to the list, save to DB. Wrapper.
         internal static void AddAccount(string UN, string EM, string ZP, string YT)
         {
             Accounts.Add(new Account(UN, EM, ZP, YT));
             _Save();
         }
 
+        // Get username, return Account object.
         internal static Account GetAccount(string UName)
         {
             foreach (Account acc in Accounts)
@@ -49,7 +80,7 @@ namespace HPQ_Project
 
         // SECOND ORDER MANAGEMENT //
 
-        // Interfacing methods
+        // Wrappers
         internal static void _Save()
         {
             SaveAccs(Accounts);
@@ -62,12 +93,14 @@ namespace HPQ_Project
 
         // DATABASE INTERACTION //
 
+        // VERY IMPORTANT: USE Path.GetFullPath(dbPath) instead of dbPath, otherwise it will save to Documents instead of the source file.
+
         // Save all accounts from the list into the database
         private static void SaveAccs(List<Account> toSave)
         {
             // Load the database
             var exApp = new Excel.Application();
-            var wb = exApp.Workbooks.Open(dbPath);
+            var wb = exApp.Workbooks.Open(Path.GetFullPath(dbPath));
 
             // Loop over all items in the accounts list
             for (int i = 0; i < toSave.Count; i++)
@@ -97,7 +130,7 @@ namespace HPQ_Project
 
             // Load up the database
             var exApp = new Excel.Application();
-            var wb = exApp.Workbooks.Open(dbPath);
+            var wb = exApp.Workbooks.Open(Path.GetFullPath(dbPath));
 
             // Loop over all values in database
             // Halt when the next value is null
